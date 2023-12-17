35b5756a0a19bc65c9d9c95acd9247db74ad540e
obalax <obalax@yahoo.com>
2023-12-17 04:27:02 +0100

    Add styles to clicker

--- clicker-app/src/App.js
+++ clicker-app/src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [title] = useState("Clicker");
   return (
     <div className="App">
+      <Header title={title} />
+      <Clicker />
     </div>
   );
 }
